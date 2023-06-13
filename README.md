bfda0a1c40ecd2a99b6393b1ecb97d2a234b10ad
Ricardo <ric.dalvarez@hotmail.com>
2023-06-13 02:27:50 +0200

    post, get, logger

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # todoApp
 
+Working with : nodejs, typescript, jest, body-parser, cors, express.
+
+Nodejs-Typescript-todoApp
+
+The app work with non persistence data, meaning it wont be available after terminating the execution of the app.
+
+this step by step assume that you have node ~v19 installed
+
+After cloning the repo on you local machine navegate to terminal and go to the folder path.
+
+navigate to the backend folder and run npm i, it will install all the dependencies needed by the backend of the app.
+
 Functionalities of my todoApp
 
 - Allow adding a task to the list.
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,36 @@
-import http from "http";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { Todo } from "./model/todoSchema";
 
-const hostname = "127.0.0.1";
-const port = 3000;
+//non - persistence data
+let loggedTimes = 1;
+let todos: Todo[] = [];
 
-const server = http.createServer((req: any, res: any) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello mieke");
+function myLogger(req: Request, res: Response, next: Function) {
+  console.log(`logged ${loggedTimes}`);
+  loggedTimes += 1;
+  next();
+}
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(myLogger);
+
+//rounting to controller functions
+app.get("/", (req: Request, res: Response) => {
+  res.json(todos);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.post("/todos", (req: Request, res: Response) => {
+  const { id, title, completed } = req.body;
+  const todo = new Todo(id, title, completed);
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
